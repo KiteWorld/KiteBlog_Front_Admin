a7a78d5ab1d135a3941d1a863d31a5c3056d03b7
Kite1874 <kite18741998@163.com>
2020-11-29 22:22:43 +0800

    完成后台登录放回token

--- src/main.js
+++ src/main.js
@@ -1,8 +1,27 @@
 import Vue from 'vue'
 import App from './App.vue'
+import router from './router/index'
+import 'normalize.css' //清楚自带样式
+import '@/plugins/elementui.js'
+import 'element-ui/lib/theme-chalk/index.css'; //elementui 样式
+import '@/theme/app.less'
+import axios from 'axios'
+
+axios.defaults.baseURL = "http://localhost:1874"
 
 Vue.config.productionTip = false
 
+axios.interceptors.request.use(function (config) {
+  console.log(config)
+  return config;
+}, function (error) {
+  console.log(error)
+  return Promise.reject(error);
+});
+
+
+
 new Vue({
   render: h => h(App),
+  router
 }).$mount('#app')
